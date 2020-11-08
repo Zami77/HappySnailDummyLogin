e1a04e48e9a0a6cc4687fa59a2e78a6e7254d68e
Dan <Chunk_77@hotmail.com>
2020-11-07 22:00:23 -0800

    Init Commit
    A dummy login to show how to create a global variable.

---
+++ HappySnailDummyLogin/Data/LoginState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HappySnailDummyLogin.Data
+{
+    public class LoginState
+    {
+        public bool IsLoggedIn { get; set; }
+        public string username { get; set; }
+        public event Action OnChange;
+
+        public void SetLogin(bool login, string user)
+        {
+            IsLoggedIn = login;
+            username = user;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}
